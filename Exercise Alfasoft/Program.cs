e07bfcaf0680ec3765ef54df567909bd7478bceb
Bruno Santos <34127252+BrunoSantos-Git@users.noreply.github.com>
2020-02-11 09:41:35 +0000

    First Commit

---
+++ Exercise Alfasoft/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercise_Alfasoft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
