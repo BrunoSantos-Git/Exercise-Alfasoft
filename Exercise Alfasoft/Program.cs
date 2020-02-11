5c51d8d110b3ecb0080144ced7e0b2d56d5505c9
Bruno Santos <34127252+BrunoSantos-Git@users.noreply.github.com>
2020-02-11 10:37:29 +0000

    Update
    New Feature: Retrieve from the bitbucket api information for each user in file

--- Exercise Alfasoft/Program.cs
+++ Exercise Alfasoft/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading;
 
 namespace Exercise_Alfasoft
 {
@@ -8,7 +11,7 @@ namespace Exercise_Alfasoft
         static void Main(string[] args)
         {
             bool canContinue;
-            string path = "";
+            string path;
             do
             {
                 Console.WriteLine("Input users file path:");
@@ -24,16 +27,42 @@ namespace Exercise_Alfasoft
 
             } while (!canContinue);
 
+            // Read all lines of file
             string[] users = File.ReadAllLines(path);
-            Console.WriteLine("Users in file:");
+
             foreach (var user in users)
             {
-                Console.WriteLine(user);
+                string url = "https://api.bitbucket.org/2.0/users/" + user;
+                var request = WebRequest.Create(url) as HttpWebRequest;
+
+                Console.WriteLine("===============================================================================================");
+                Console.WriteLine("User being retrieved: " + user);
+                Console.WriteLine("URL: " + url);
+
+                using (var response = request.GetResponse() as HttpWebResponse)
+                {
+                    var reader = new StreamReader(response.GetResponseStream());
+                    string json = reader.ReadToEnd();
+                    Console.WriteLine("Output Request: \n" + json);
+                }
+                Console.WriteLine("===============================================================================================");
+
+                if (user != users.Last()) // If is not the last user in list
+                    Thread.Sleep(5000);   // Wait 5 seconds
             }
 
-            Console.ReadLine();
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey(true); // Do not display the pressed key
+            Console.WriteLine("Application will close in 5 seconds");
+            Thread.Sleep(5000);
+            Environment.Exit(0);
         }
 
+        /// <summary>
+        /// Check if a path is valid and is a text file
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns>True if path is valid and False if path is not valid</returns>
         private static bool ValidPath(string path)
         {
             if (!File.Exists(path))
