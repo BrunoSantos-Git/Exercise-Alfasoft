67c136df4df9ea133fd39b9bc37305deaff1b6e6
Bruno Santos <34127252+BrunoSantos-Git@users.noreply.github.com>
2020-02-11 10:09:14 +0000

    Update
    Updated: Input file path and read users from file

--- Exercise Alfasoft/Program.cs
+++ Exercise Alfasoft/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Exercise_Alfasoft
 {
@@ -6,7 +7,48 @@ namespace Exercise_Alfasoft
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool canContinue;
+            string path = "";
+            do
+            {
+                Console.WriteLine("Input users file path:");
+                path = Console.ReadLine();
+
+                if (!ValidPath(path))
+                {
+                    Console.WriteLine("\nInvalid File!\n");
+                    canContinue = false;
+                }
+                else
+                    canContinue = true;
+
+            } while (!canContinue);
+
+            string[] users = File.ReadAllLines(path);
+            Console.WriteLine("Users in file:");
+            foreach (var user in users)
+            {
+                Console.WriteLine(user);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static bool ValidPath(string path)
+        {
+            if (!File.Exists(path))
+            {
+                return false;
+            }
+            else
+            {
+                if (Path.GetExtension(path) != ".txt")
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
